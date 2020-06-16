c8bdd9751267be3d8ab94614b09d9d83c6f472ef
chages16 <chages16@gmail.com>
2020-06-16 23:28:58 +0930

    Created local storage options and ability to save data. Created clear all function and function to offer to clear on a new day

--- script/script.js
+++ script/script.js
@@ -1,11 +1,10 @@
-var today = new Date();
-var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 var hournow = parseInt(moment().format('H'));
 var classes = [".8AM", ".9AM", ".10AM", ".11AM", ".12PM", ".1PM", ".2PM", ".3PM", ".4PM", ".5PM", ".6PM"]
 var time = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
-console.log(hournow)
 var classindex = time.indexOf(hournow);
 $("#currentDay").text(moment.utc().format('dddd, MMMM D, YYYY'));
+var currentdaycheck = moment.utc().format('dddd, MMMM D, YYYY')
+var allnotes = ["", "", "", "", "", "", "", "", "", "", ""]
 
 if (hournow < 9) {
     allfuture();
@@ -18,16 +17,14 @@ else {
 }
 
 function allfuture() {
-    for (i = 1; i < time.length; i++) {
-        console.log(classes[i])
+    for (i = 1; i < classes.length; i++) {
         $(classes[i]).addClass("future");
     }
     $(classes[0]).addClass("present");
 }
 
 function allpast() {
-    for (i = 0; i < time.length - 1; i++) {
-        console.log(classes[i])
+    for (i = 0; i < classes.length - 1; i++) {
         $(classes[i]).addClass("past");
     }
     $(classes[time.length - 1]).addClass("present");
@@ -40,5 +37,55 @@ function formattimes() {
     }
     for (i = classindex + 1; i < classes.length; i++) {
         $(classes[i]).addClass("future");
+    }
+}
+
+$(".saveBtn").on("click", function () {
+    var di = $(this).data('index');
+    allnotes[di] = $(classes[di]).val();
+    localStorage.setItem('allnotes', JSON.stringify(allnotes))
+    alert("saved")
+
+})
+
+grabdata();
+
+function grabdata() {
+    allnotes = JSON.parse(localStorage.getItem("allnotes"));
+    if (allnotes == null) {
+        allnotes = ["", "", "", "", "", "", "", "", "", "", ""];
+        return;
+    }
+    for (i = 0; i < classes.length; i++) {
+        ($(classes[i])).val(allnotes[i]);
+    }
+}
+
+$(".cleary").on("click", function () {
+    cleardata()
+})
+function cleardata() {
+    var confirmdelete = confirm("Are you sure you want to clear all data");
+    if (confirmdelete == true) {
+        allnotes = ["", "", "", "", "", "", "", "", "", "", ""];
+        localStorage.setItem('allnotes', JSON.stringify(allnotes));
+        grabdata();
+    }
+}
+
+checkday()
+function checkday() {
+    var dateset = localStorage.getItem("date");
+    if (dateset == null) {
+        localStorage.setItem('date', currentdaycheck);
+    }
+    else if (currentdaycheck !== dateset) {
+        localStorage.setItem('date', currentdaycheck);
+        var confirmnewday = confirm("It's a new day, would you like to clear the calendar?")
+        if (confirmnewday == true) {
+            cleardata()
+            localStorage.setItem('date', currentdaycheck);
+
+        }
     }
 }
