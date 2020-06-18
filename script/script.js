fa2228193e9c3f9848953dbbb874bd8172f14d00
chages16 <chages16@gmail.com>
2020-06-18 21:32:26 +0930

    Fixed variable names

--- script/script.js
+++ script/script.js
@@ -1,29 +1,29 @@
 //list of global variables
-var hournow = parseInt(moment().format('H'));
+var hourNow = parseInt(moment().format('H'));
 var classes = [".8AM", ".9AM", ".10AM", ".11AM", ".12PM", ".1PM", ".2PM", ".3PM", ".4PM", ".5PM", ".6PM"]
 var time = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
-var classindex = time.indexOf(hournow);
+var classIndex = time.indexOf(hourNow);
 $("#currentDay").text(moment().format('dddd, MMMM D, YYYY'));
-var currentdaycheck = moment().format('dddd, MMMM D, YYYY')
-var allnotes = ["", "", "", "", "", "", "", "", "", "", ""]
+var currentDayCheck = moment().format('dddd, MMMM D, YYYY')
+var allNotes = ["", "", "", "", "", "", "", "", "", "", ""]
 
 //startup code to designate past, future and present classes to container rows
-if (hournow < 9) {
-    allfuture();
+if (hourNow < 9) {
+    allFuture();
 }
-else if (hournow > 17) {
-    allpast();
+else if (hourNow > 17) {
+    allPast();
 }
 else {
-    formattimes()
+    formatTimes()
 }
 
 //startup functions to grab data from local storage data and to determine if app opened on a new day
-grabdata();
-checkday()
+grabData();
+checkDay()
 
 /// function for when time is before 9am, assigning all but the first row the future class
-function allfuture() {
+function allFuture() {
     for (i = 1; i < classes.length; i++) {
         $(classes[i]).addClass("future");
     }
@@ -31,7 +31,7 @@ function allfuture() {
 }
 
 /// function for when time is after 6pm, assigning all but the last row the past class
-function allpast() {
+function allPast() {
     for (i = 0; i < classes.length - 1; i++) {
         $(classes[i]).addClass("past");
     }
@@ -39,34 +39,34 @@ function allpast() {
 }
 
 /// function for middle of the day- uses moment api to determine the hour and assigns classes by that
-function formattimes() {
-    $(classes[classindex]).addClass("present");
-    for (i = 0; i < classindex; i++) {
+function formatTimes() {
+    $(classes[classIndex]).addClass("present");
+    for (i = 0; i < classIndex; i++) {
         $(classes[i]).addClass("past");
     }
-    for (i = classindex + 1; i < classes.length; i++) {
+    for (i = classIndex + 1; i < classes.length; i++) {
         $(classes[i]).addClass("future");
     }
 }
 
-//save button function, stores in allnotes array and local storage data
+//save button function, stores in allNotes array and local storage data
 $(".saveBtn").on("click", function () {
     var di = $(this).data('index');
-    allnotes[di] = $(classes[di]).val();
-    localStorage.setItem('allnotes', JSON.stringify(allnotes))
+    allNotes[di] = $(classes[di]).val();
+    localStorage.setItem('allNotes', JSON.stringify(allNotes))
     alert("saved")
 
 })
 
-//grabdata function, restores data to rows from local storage
-function grabdata() {
-    allnotes = JSON.parse(localStorage.getItem("allnotes"));
-    if (allnotes == null) {
-        allnotes = ["", "", "", "", "", "", "", "", "", "", ""];
+//grabData function, restores data to rows from local storage
+function grabData() {
+    allNotes = JSON.parse(localStorage.getItem("allNotes"));
+    if (allNotes == null) {
+        allNotes = ["", "", "", "", "", "", "", "", "", "", ""];
         return;
     }
     for (i = 0; i < classes.length; i++) {
-        ($(classes[i])).val(allnotes[i]);
+        ($(classes[i])).val(allNotes[i]);
     }
 }
 
@@ -77,26 +77,26 @@ $(".cleary").on("click", function () {
 
 //clear data function, clears allnote array, all rows and stores empty array in local storage
 function cleardata() {
-    var confirmdelete = confirm("Are you sure you want to clear all data");
-    if (confirmdelete == true) {
-        allnotes = ["", "", "", "", "", "", "", "", "", "", ""];
-        localStorage.setItem('allnotes', JSON.stringify(allnotes));
-        grabdata();
+    var confirmDelete = confirm("Are you sure you want to clear all data");
+    if (confirmDelete == true) {
+        allNotes = ["", "", "", "", "", "", "", "", "", "", ""];
+        localStorage.setItem('allNotes', JSON.stringify(allNotes));
+        grabData();
     }
 }
 
 //function that compares current day with last day stored in local storage. offers user to clear data if day has changed.
-function checkday() {
-    var dateset = localStorage.getItem("date");
-    if (dateset == null) {
-        localStorage.setItem('date', currentdaycheck);
+function checkDay() {
+    var dateSet = localStorage.getItem("date");
+    if (dateSet == null) {
+        localStorage.setItem('date', currentDayCheck);
     }
-    else if (currentdaycheck !== dateset) {
-        localStorage.setItem('date', currentdaycheck);
-        var confirmnewday = confirm("It's a new day, would you like to clear the calendar?")
-        if (confirmnewday == true) {
+    else if (currentDayCheck !== dateSet) {
+        localStorage.setItem('date', currentDayCheck);
+        var confirmNewDay = confirm("It's a new day, would you like to clear the calendar?")
+        if (confirmNewDay == true) {
             cleardata()
-            localStorage.setItem('date', currentdaycheck);
+            localStorage.setItem('date', currentDayCheck);
         }
     }
 }
