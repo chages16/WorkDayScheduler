8152255d5f056e87c0bfd7295fb4b9a556730122
chages16 <chages16@gmail.com>
2020-06-18 19:09:58 +0930

    fixed moment() function to display correct time

--- script/script.js
+++ script/script.js
@@ -3,8 +3,8 @@ var hournow = parseInt(moment().format('H'));
 var classes = [".8AM", ".9AM", ".10AM", ".11AM", ".12PM", ".1PM", ".2PM", ".3PM", ".4PM", ".5PM", ".6PM"]
 var time = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
 var classindex = time.indexOf(hournow);
-$("#currentDay").text(moment.utc().format('dddd, MMMM D, YYYY'));
-var currentdaycheck = moment.utc().format('dddd, MMMM D, YYYY')
+$("#currentDay").text(moment().format('dddd, MMMM D, YYYY'));
+var currentdaycheck = moment().format('dddd, MMMM D, YYYY')
 var allnotes = ["", "", "", "", "", "", "", "", "", "", ""]
 
 //startup code to designate past, future and present classes to container rows
